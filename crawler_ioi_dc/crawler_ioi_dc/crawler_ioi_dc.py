399701d8a2c0fcbc36e2b90b053d2b4b9fa4d071
Hurderella <hurderella@gmail.com>
2016-04-23 14:55:36 +0900

    Base logic

--- crawler_ioi_dc/crawler_ioi_dc/crawler_ioi_dc.py
+++ crawler_ioi_dc/crawler_ioi_dc/crawler_ioi_dc.py
@@ -1,1 +1,25 @@
+#-*- coding: utf-8 -*-
+import BeautifulSoup
+import httplib
+import urllib
 
+# http://gall.dcinside.com/board/lists/?id=kimsohye
+
+sohye = "http://gall.dcinside.com/board/lists/?id=kimsohye"
+daum = "http://www.daum.net"
+
+if __name__ == "__main__":
+	print("Hi DC");
+
+	data = urllib.urlopen(sohye).read()
+	print(data)
+	f = open("./urldump.txt", "w")
+	f.write(data)
+	f.close()
+
+#	conn = httplib.HTTPConnection(daum, 8080)
+	#conn.request("POST", "/board/lists/?id=kimsohye")
+	#conn.request("GET", "/")
+#	data = conn.getresponse()#
+	#print(data.read())
+	#conn.close()
