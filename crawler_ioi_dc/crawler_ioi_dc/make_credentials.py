df6e68ed822bf4e17b58c1ab40f32a1c62cbc63f
Hurderella <hcs3759@gmail.com>
2016-04-30 23:57:33 +0900

    add token func & img upload to imgur

--- crawler_ioi_dc/crawler_ioi_dc/make_credentials.py
+++ crawler_ioi_dc/crawler_ioi_dc/make_credentials.py
@@ -3,24 +3,45 @@ import urllib2
 import sys
 import getopt
 
+
 def main(argv):
+	
+	client_id = '7bd202e0f6253f8'
+	client_secret = 'e8d018d24bf8e44d598f146c4b5a84e90cfb8c36'
+	pin = ""
+
+	client = ImgurClient(client_id, client_secret)
+	authorization_url = client.get_auth_url('pin')
 	usage = "usage : make_credentials.py -p <pin number>"
-	pin = 0
+	usage += "\nauthorization url : \n\t" + authorization_url
+	
 	try:
 		opts, args = getopt.getopt(argv, "hp:", ["--pin="])
 		
 		for opt, arg in opts:
 			if opt == "-h":
 				print(usage)
+				sys.exit(2)
 			elif opt in ("-p", "--pin="):
-				pin = int(arg)
+				pin = arg
+		
+		credentials = client.authorize(pin, 'pin')
+		access_token = "access : " + credentials['access_token']
+		refresh_token = "refres : " + credentials['refresh_token']
+		print(access_token)
+		print(refresh_token)
+		keys = open("./token.txt", "w")
+		keys.write(access_token + "\r\n")
+		keys.write(refresh_token)
+		keys.close()
 	except getopt.GetoptError:
 		print(usage)
-	except ValueError as ve:
-		print(usage)
-		print("error : " + ve.message)
+	
 		
 		
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+	try:
+		main(sys.argv[1:])
+	except:
+		print("type : make_credentials.py -h")
